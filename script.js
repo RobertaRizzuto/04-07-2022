0538dfc750366a23bcac69543abc20577ebfe7d2
RobertaRizzuto <rtarizzuto@gmail.com>
2022-07-04 22:59:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+(function () {
+  const studentName = {
+    name1: "roberta",
+    name2: "luca",
+    name3: "richard",
+    name4: "marco",
+  };
+
+  const studentOrder = {
+    number1: 1,
+    number2: 2,
+    number3: 3,
+    number4: 4,
+  };
+
+  const studentList = { ...studentName, ...studentOrder };
+  console.log(studentList);
+  // secondo esercizio
+
+  const classroom = ["A", "B", "C", "D"];
+
+  const studentClass = classroom.map(function (lettera) {
+    const studentNick = { ...studentList, lettera };
+
+    studentNick.lettera = lettera;
+
+    return studentNick;
+  });
+
+  console.log(studentClass);
+
+  /// Destructuring
+
+  const array = ["nonsocosascrivere", "Lucia", "Lorena", "ignotissimo"].filter(
+    (el) => el.length > 10
+  );
+  console.log(array);
+})();
+
+
+(() => {
+  const shop = {
+    _products: [
+      { id: 1, name: "TV", price: 40 },
+      { id: 2, name: "PC", price: 30 },
+    ],
+    get products() {
+      return this._products;
+    },
+    set products(value) {
+      [this.id, this.name, this.price] = value.split(" ");
+      document.body.innerHTML = `
+<ul class="product-list">
+<li>Id: ${shop.products[0].id}</li>
+<li>Name: ${shop.products[0].name}</li>
+<li>Price: ${shop.products[0].price}€</li>
+</ul>
+<ul class="product-list">
+<li>Id: ${shop.products[1].id}</li>
+<li>Name: ${shop.products[1].name}</li>
+<li>Price: ${shop.products[1].price}€</li>
+</ul>
+
+`;
+    },
+  };
+
+  console.log(shop._products);
+  shop.products = ' 1 phon 28 ';
+  shop.products = ' 2 radio 32 ';
+  console.log(shop._products);
+})();
+
+
+
